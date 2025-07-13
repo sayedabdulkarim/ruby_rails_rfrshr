2ebf936bac0b9c01e4572838dfd16324a689741a
Sayed Abdul <sakarim9124@gmail.com>
2025-07-13 15:50:26 +0530

    Refactor routes and update home#index view with navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get "pages/about_us"
-  get "pages/contact_us"
-  get "pages/privacy_policy"
-  # get "pages/terms_and_conditions"
-  get "/pages/terms" => "pages#terms_and_conditions"
+  get "pages/about_us", as: "about"
+  get "pages/contact_us", as: "contact"
+  get "pages/privacy_policy", as: "privacy"
+  get "/pages/privacy_policy" => "pages#terms_and_conditions", as: "terms"
+  root 'home#index' # this 'root' sets the / URL
+
+
+  ## The following are examples of how to set up routes for the home page.
   # get 'home/index'
   # get '/home' => 'home#index'
 
   # get 'home', to: 'home#index'
 
-  root 'home#index' # this 'root' sets the / URL
 
 end
