8aab4fc486bd1c306d71483071915a1251bba2a5
Sayed Abdul <sakarim9124@gmail.com>
2025-07-13 14:28:55 +0530

    Add pages controller and views for about, contact, privacy, and terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "pages/about_us"
+  get "pages/contact_us"
+  get "pages/privacy_policy"
+  # get "pages/terms_and_conditions"
+  get "/pages/terms" => "pages#terms_and_conditions"
   # get 'home/index'
   # get '/home' => 'home#index'
 
