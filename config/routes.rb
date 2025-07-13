0db72ba984a2882b96a1d72bd9fb5e667bb8600b
Sayed Abdul <sakarim9124@gmail.com>
2025-07-13 13:24:45 +0530

    comment out 'home' route and ensure root route is set for home#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # get 'home/index'
   # get '/home' => 'home#index'
 
-  get 'home', to: 'home#index'
+  # get 'home', to: 'home#index'
+
+  root 'home#index' # this 'root' sets the / URL
+
 end
