d894c77ef26d889cb360d7adb3770308b7eba33a
Sayed Abdul <sakarim9124@gmail.com>
2025-07-14 21:51:57 +0530

    Refactor employee parameters and validations; enhance error display in new employee form

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -41,8 +41,8 @@ class EmployeesController < ApplicationController
   end
 
 
-  def employee_params
-    params.require(:employee).permit(:name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
-  end
+def employee_params
+  params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
+end
 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,7 @@
 class Employee < ApplicationRecord
-    # validates :name, presence: true
-    # validates :personal_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    # validates :city, presence: true
-    # validates :state, presence: true
-    # validates :country, presence: true
-    # validates :pincode, presence: true, numericality: { only_integer: true }
-    # validates :address_line_1, presence: true
-    # validates :address_line_2, presence: true
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 
     def employee_name(o)
         "#{o.first_name} #{o.last_name}".strip
