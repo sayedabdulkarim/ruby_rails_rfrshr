9273df01aca8c7f83d55c17c251f830259b97aad
Sayed Abdul <sakarim9124@gmail.com>
2025-07-15 23:32:52 +0530

    Refactor employee controller: implement set_employee method and comment out direct find calls

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,11 +1,13 @@
 class EmployeesController < ApplicationController
-
+  
+  ## we can create a single function which is common in all action
+  before_action :set_employee, only: [:show, :edit, :update, :destroy] 
   def index
     @employees = Employee.all
   end
 
   def show
-    @employee = Employee.find(params[:id])
+    # @employee = Employee.find(params[:id])
   end
 
   def new
@@ -22,11 +24,11 @@ class EmployeesController < ApplicationController
   end
 
   def edit
-    @employee = Employee.find(params[:id])
+    # @employee = Employee.find(params[:id]) 
   end
 
 def update
-  @employee = Employee.find(params[:id])
+  # @employee = Employee.find(params[:id])
   if @employee.update(employee_params)
     redirect_to employees_path, notice: 'Employee was successfully updated.'
   else
@@ -35,7 +37,7 @@ def update
 end
 
   def destroy
-    @employee = Employee.find(params[:id])
+    # @employee = Employee.find(params[:id])
     @employee.destroy
     redirect_to employees_path, notice: 'Employee was successfully destroyed.'
   end
@@ -43,6 +45,14 @@ end
 
 def employee_params
   params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
+end
+
+private
+
+def set_employee
+  @employee = Employee.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => error
+  redirect_to employees_path, notice: error
 end
 
 end
