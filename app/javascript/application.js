4ca4b22d09c02565be1603c69da981bc75bc418c
Sayed Abdul <sakarim9124@gmail.com>
2025-07-15 23:16:07 +0530

    Enhance employee management views: add show page, improve index layout, and integrate Rails UJS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import "@hotwired/turbo-rails"
-import "controllers";
+// import "controllers";
+import Rails from "rails-ujs"; // ✅ Match the pin name
+Rails.start();
